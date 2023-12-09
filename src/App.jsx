368448ff5e3be2bec0cac50f9a34641633204400
Waldo <oswaldo.ct18@gmail.com>
2023-12-09 05:10:41 -0600

    Centered the logo and rearranged the gifs so it looks cooler

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,94 @@
-import React, { useRef } from 'react'
-import kivotos from './images/Kivotos_1.webp'
-import logo from './images/WaldoArchive_ba-style@nulla.top.png'
-import reisa from './images/reisa-dance-blue-archive.gif'
-import arisu from './images/arisu-blue-archive.gif'
-import sweetclub from './images/sweetclub.jpg'
+import React, { useRef } from "react";
+import kivotos from "./images/Kivotos_1.webp";
+import logo from "./images/WaldoArchive_ba-style@nulla.top.png";
+import reisa from "./images/reisa-dance-blue-archive.gif";
+import arisu from "./images/arisu-blue-archive.gif";
+import sweetclub from "./images/sweetclub.jpg";
 
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function App() {
   const ref = useRef();
 
   return (
     <>
-      <Parallax pages={4} ref={ref}>
-
-        <ParallaxLayer 
-        speed={0.2}
-        offset={0}
-        factor={1.5}
-        style={{
-           backgroundImage: `url(${kivotos})`,
-           backgroundSize: 'cover'
-        }}
-        >
-        </ParallaxLayer>
-
-        <ParallaxLayer 
-        offset={0.2} 
-        speed={1}
-        factor={1}
-        style={{
-          backgroundImage: `url(${logo})`,}}
-          >
-        </ParallaxLayer>
-          
+      <Parallax pages={1.5} ref={ref}>
         <ParallaxLayer
-        speed={0.2}
-        offset={3}
-        factor={1}
-        style={{
-           backgroundImage: `url(${sweetclub})`,
-           backgroundSize: 'cover'
-        }}>
-        </ParallaxLayer>
+          speed={0.9}
+          offset={0}
+          factor={2}
+          style={{
+            backgroundImage: `url(${kivotos})`,
+            backgroundSize: "cover",
+          }}
+        ></ParallaxLayer>
 
         <ParallaxLayer
-        sticky={{ start: 0.17, end: 2.9 }}
-        onClick={()=> ref.current.scrollTo(0)}
-        className='ml-24'
+          offset={0}
+          speed={0}
+          factor={1}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
         >
-          <img src={reisa} alt="" />
+          <img
+            src={logo}
+            alt="Logo"
+            style={{
+              maxWidth: "70%",
+              maxHeight: "30%",
+              height: "auto",
+              width: "auto",
+              minWidth: "140px",
+              minHeight: "60px",
+            }}
+            className="my-28"
+          />
         </ParallaxLayer>
 
         <ParallaxLayer
-        offset={0.17} 
-        speed={1}
-        onClick={()=> ref.current.scrollTo(3)}
+          sticky={{ start: 0.33, end: 0.7 }}
+          onClick={() => ref.current.scrollTo(0)}
+          speed={7}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
         >
-          <img src={arisu} alt="" />
+          <img
+            src={reisa}
+            alt=""
+            style={{
+              height: "128px",
+              width: "128px",
+            }}
+            className="ml-[50%]"
+          />
         </ParallaxLayer>
 
-        
+        <ParallaxLayer
+          offset={0.1}
+          speed={0}
+          onClick={() => ref.current.scrollTo(2)}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <img
+            src={arisu}
+            alt=""
+            style={{
+              height: "128px",
+              width: "128px",
+            }}
+            className="mr-[50%]"
+          />
+        </ParallaxLayer>
       </Parallax>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
